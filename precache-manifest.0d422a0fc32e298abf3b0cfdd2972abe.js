d3cdc2ad437a55d9f3a3353c2b215208e81d8304
Dejan85 <xypnotica@gmail.com>
2019-12-20 14:23:35 +0100

    Updates

---
+++ precache-manifest.0d422a0fc32e298abf3b0cfdd2972abe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c1f3db9222f1005b27c81ff4e34b5b",
+    "url": "/DF-Immobilien/index.html"
+  },
+  {
+    "revision": "c712195b5a45520788e5",
+    "url": "/DF-Immobilien/static/js/2.ed7ecd77.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/DF-Immobilien/static/js/2.ed7ecd77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45c057ed6d0ba2a8e089",
+    "url": "/DF-Immobilien/static/js/main.f36936dc.chunk.js"
+  },
+  {
+    "revision": "e429cfd55e2957037f13",
+    "url": "/DF-Immobilien/static/js/runtime-main.726486df.js"
+  }
+]);
