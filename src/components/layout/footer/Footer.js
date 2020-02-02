2c7323d2baf592dff9e34e0b3af100e4981cadd3
Dejan85 <xypnotica@gmail.com>
2020-02-02 11:39:45 +0100

    footer done

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,21 +1,155 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <ul className="footer__ul">
-        <li className="footer__li">
-          <Link to="/dfimmobilien/impressum" className="footer__link">
-            Impressum
-          </Link>
-        </li>
-        <li className="footer__li">
-          <Link to="/dfimmobilien/datenschutz" className="footer__link">
-            Datenschutz
-          </Link>
-        </li>
-      </ul>
+      <div className="footer__content">
+        <div className="footer__sometext">
+          <h1 className="footer__h1">Ltes talk about</h1>
+          <p className="footer__p">
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts. Far far away,
+            behind the word mountains, far from the countries Vokalia and
+            Consonantia, there live the blind texts.
+          </p>
+        </div>
+        <div className="footer__links">
+          <h1 className="footer__h1">Links</h1>
+          <ul className="footer__ul">
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="home"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                Home
+              </ScrollLink>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="überuns"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                >Über uns
+              </ScrollLink>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="unsereLeistungen"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                Unsere Leistungen
+              </ScrollLink>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="AktuelleAngebote"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                Aktuelle Angebote
+              </ScrollLink>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="immobilienankauf"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                Immobilienankauf
+              </ScrollLink>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="referenzen"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                Referenzen
+              </ScrollLink>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-long-arrow-alt-right" />
+              <ScrollLink
+                className="footer__link"
+                to="kontakt"
+                spy={true}
+                smooth={"true"}
+                duration={1500}
+                offset={-100}
+              >
+                Kontakt
+              </ScrollLink>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__kontakt">
+          <h1 className="footer__h1">Kontakt</h1>
+          <ul className="footer__ul">
+            <li className="footer__li">
+              <i className="far fa-map" />
+              <p> 91058 Erlangen</p>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-user-tie" />
+              <p> Dejan Bogojevic, Geschäftsführer</p>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-phone  footer__icon" />
+              <p>09131/611581</p>
+            </li>
+            <li className="footer__li">
+              <i className="fas fa-envelope footer__icon" />
+              <p>info@df-immobilien.de</p>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__impressum">
+          <ul className="footer__ul">
+            <li className="footer__li">
+              <Link to="/dfimmobilien/impressum" className="footer__link">
+                Impressum
+              </Link>
+            </li>
+            <li className="footer__li">
+              <Link to="/dfimmobilien/datenschutz" className="footer__link">
+                Datenschutz
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer__copyright">
+        DF Immobilien ©2020 by Dejan Markovic
+      </div>
     </div>
   );
 };
--- src/components/layout/kontakt/Kontakt.js
+++ src/components/layout/kontakt/Kontakt.js
@@ -4,18 +4,6 @@ import Map from "./Map";
 const Kontakt = () => {
   return (
     <div className="kontakt">
-      {/* <h1 className="kontakt__h1">Kontakt</h1> */}
-      {/* <p className="kontakt__p">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p> */}
       <div className="kontakt__content">
         <div className="kontakt__leftside">
           <h1 className="kontakt__h1">Kontakt</h1>
